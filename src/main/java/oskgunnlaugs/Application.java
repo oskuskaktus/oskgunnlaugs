cbecbe994f037487e88c84d8d8e5ea66ecd6f69d
osk <osk@localhost>
2022-07-09 16:23:43 +0000

    Initial import

---
+++ src/main/java/oskgunnlaugs/Application.java
@@ -0,0 +1,10 @@
+package oskgunnlaugs;
+
+import ng.appserver.NGApplication;
+
+public class Application extends NGApplication {
+
+	public static void main(String[] args) {
+		NGApplication.run(args, Application.class );
+	}
+}
