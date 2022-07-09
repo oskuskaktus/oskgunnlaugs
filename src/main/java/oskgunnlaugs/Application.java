2794fd167d72cdf240aab0a9683084b81b477580
osk <osk@localhost>
2022-07-09 16:25:24 +0000

    Add front page

--- src/main/java/oskgunnlaugs/Application.java
+++ src/main/java/oskgunnlaugs/Application.java
@@ -1,10 +1,17 @@
 package oskgunnlaugs;
 
+import ng.appserver.NGActionResults;
 import ng.appserver.NGApplication;
+import ng.appserver.NGRequest;
 
 public class Application extends NGApplication {
 
 	public static void main(String[] args) {
-		NGApplication.run(args, Application.class );
+		NGApplication.run(args, Application.class);
+	}
+
+	@Override
+	public NGActionResults defaultResponse(NGRequest request) {
+		return pageWithName(Main.class, request.context());
 	}
 }
---
+++ src/main/java/oskgunnlaugs/Main.java
@@ -0,0 +1,11 @@
+package oskgunnlaugs;
+
+import ng.appserver.NGComponent;
+import ng.appserver.NGContext;
+
+public class Main extends NGComponent {
+
+	public Main(NGContext context) {
+		super(context);
+	}
+}
