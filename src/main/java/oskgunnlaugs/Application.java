781ca0c77842329487dbcf6b3a705361124cc035
osk <osk@localhost>
2022-07-09 16:39:48 +0000

    Added wrapper component

--- src/main/java/oskgunnlaugs/Application.java
+++ src/main/java/oskgunnlaugs/Application.java
@@ -1,13 +1,18 @@
-package oskgunnlaugs;
+	package oskgunnlaugs;
 
 import ng.appserver.NGActionResults;
 import ng.appserver.NGApplication;
 import ng.appserver.NGRequest;
+import ng.appserver.templating._NGUtilities;
 
 public class Application extends NGApplication {
 
 	public static void main(String[] args) {
 		NGApplication.run(args, Application.class);
+	}
+	
+	public Application() {
+		_NGUtilities.addClass( Wrapper.class );
 	}
 
 	@Override
---
+++ src/main/java/oskgunnlaugs/Wrapper.java
@@ -0,0 +1,11 @@
+package oskgunnlaugs;
+
+import ng.appserver.NGComponent;
+import ng.appserver.NGContext;
+
+public class Wrapper extends NGComponent {
+
+	public Wrapper(NGContext context) {
+		super(context);
+	}
+}
